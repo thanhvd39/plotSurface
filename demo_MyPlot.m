c89dc22dee9f4bed6cc6f82d5c972c517672f86b
thanh <tth@thanhs-MacBook-Air.local>
2024-03-07 14:01:56 +0700

    update atlas

--- demo_MyPlot.m
+++ demo_MyPlot.m
@@ -1,26 +1,30 @@
 
 clc,
-% close all;
+close all;
 clear;
 
+load('/Users/tth/Thanh/plotSurface/gender_test.mat')
+% load('/Users/tth/Thanh/plotSurface/AD_MCI_test.mat')
 
 load('mySurface_data.mat')
-load('D:\Thanh\Research\ADNI\figure\figure 5\PQ_classification.mat')
+% PQ = rand(200,3);
+% load('D:\Thanh\Research\ADNI\figure\figure 5\PQ_classification.mat')
 % load('D:\Thanh\Research\rePLS-figure\figure5\PQ_classification_3.mat')
 
-for i=1:3
-difference_PQ = zeros(size(PQ));
-difference_PQ(:,1) = PQ(:,2) - PQ(:,1);
-difference_PQ(:,2) = PQ(:,3) - PQ(:,2);
-difference_PQ(:,3) = PQ(:,3) - PQ(:,1);
-% difference_PQ = -difference_PQ;
-corticalThicknesses = difference_PQ(:,i);
+% for i=1:1 
+% difference_PQ = zeros(size(PQ));
+% difference_PQ(:,1) = PQ(:,2) - PQ(:,1);
+% difference_PQ(:,2) = PQ(:,3) - PQ(:,2);
+% difference_PQ(:,3) = PQ(:,3) - PQ(:,1);
+% difference_PQ = PQ;
+% corticalThicknesses = difference_PQ(:,i);
 
-% corticalThicknesses = randn(202,1);
-cmin = min(difference_PQ(:));
-cmax = max(difference_PQ(:));
-% cmin = -0.04;
-% cmax = 0.02;
+% corticalThicknesses = PQ';%randn(202,1);
+corticalThicknesses = reshape(PQ,[202,1]);
+cmin = min(corticalThicknesses(:));
+cmax = max(corticalThicknesses(:));
+% cmin = 2;
+% cmax =0 ;
 corticalThicknesses(1) = NaN;
 corticalThicknesses(2) = NaN;
 
@@ -28,9 +32,9 @@ corticalThicknesses(2) = NaN;
 [left_cdata, lh_vertex_id, right_cdata, rh_vertex_id,final_cdata,vertex_id]  = convertCorticalThickness2VertexData(corticalThicknesses);
 data_label = "";
 middleValue = 0;
-range_thresh = 0.1;
+range_thresh = 0.05;
 cmap = mycolormap(middleValue,range_thresh,cmin,cmax);
-
+% cmap = jet(100);
 
 % surface_all.both = g_mesh;
 % surface_all.lh = g_lh;
@@ -48,12 +52,12 @@ MyExampleSurfacePlotFunction(surface_all,id_all,data_all,cmap,data_label,climits
 % ExampleSurfacePlotFunction(g_lh,lh_vertex_id,left_cdata,cmap,data_label);
 % ExampleSurfacePlotFunction(g_rh,rh_vertex_id,right_cdata,cmap,data_label);
 % ExampleSurfacePlotFunction(g_mesh,vertex_id,final_cdata,cmap,data_label);
-if i == 1
-    saveas(gcf,sprintf("PQ_MCI_HC.png", i))
-elseif i ==2
-    saveas(gcf,sprintf("PQ_AD_MCI.png", i))
-elseif i==3 
-    saveas(gcf,sprintf("PQ_AD_HC.png", i))
-end
-% break
-end
+% if i == 1
+%     saveas(gcf,sprintf("PQ_MCI_HC.png", i))
+% elseif i ==2
+%     saveas(gcf,sprintf("PQ_AD_MCI.png", i))
+% elseif i==3 
+%     saveas(gcf,sprintf("PQ_AD_HC.png", i))
+% end
+% % break
+% end
--- mycolormap.m
+++ mycolormap.m
@@ -6,10 +6,18 @@ function cmap = mycolormap(middleValue,range_thresh,cmin,cmax)
 n = 256;                %// resolution
 % c = colorbar;
 middle_index = dsearchn(linspace(cmin,cmax,n)',middleValue);
-cmap(1,:) = [220,0,0]/255; %red
+cmap(4,:) = [220,0,0]/255; %red
+% cmap(1,:) = [245,245,245]/255; %gray
 cmap(2,:) = [245,245,245]/255;   %gray
 cmap(3,:) = [247,247,247]/255;   %gray
-cmap(4,:) = [60,84,136]/255; % blue
+cmap(1,:) = [60,84,136]/255; % blue
+
+% 
+% % % cmap(1,:) = [220,0,0]/255; %red
+% cmap(1,:) = [245,245,245]/255; %gray
+% cmap(2,:) = [245,245,245]/255;   %gray
+% cmap(3,:) = [247,247,247]/255;   %gray
+% cmap(4,:) = [220,0,0]/255; % red
 
 [X,Y] = meshgrid([1:3],[1:n]);  %// mesh of indices
 cmap = interp2(X([1,middle_index-int32(n*range_thresh),middle_index+int32(n*range_thresh),n],:),Y([1,middle_index-int32(n*range_thresh),middle_index+int32(n*range_thresh),n],:),cmap,X,Y); %// interpolate colormap
